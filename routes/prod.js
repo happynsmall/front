f5b746c41687738c10cc9def75c611843065f038
syp <skyshop5210@gmail.com>
2021-04-08 19:23:46 +0900

    change nego

--- routes/prod.js
+++ routes/prod.js
@@ -57,4 +57,31 @@ let getEntry = function(id, callback) {
 	});
 }
 
+router.get("/nego/:id", (req, res) => {
+	util.log("네고 정보 조회");
+	getNegoEntry(req.params.id, function (ret) {
+		if (ret != null) {
+			res.send(ret);
+		}
+	});
+});
+
+// //--- get one data
+let getNegoEntry = function (id, callback) {
+	util.log("request->" + __API_PRODUCT_URI + "/nego/" + id);
+	axios.get(__API_PRODUCT_URI + "/nego/" + id, {
+	})
+		.then((ret) => {
+			util.log("Success to get detail info !");
+			let data = ret.data;
+			util.log(ret);
+			util.log(data);
+			callback(data);
+		})
+		.catch((error) => {
+			console.error("Fail to get product info!", error);
+			callback(null);
+		});
+}
+
 module.exports = router;
